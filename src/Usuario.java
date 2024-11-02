3bf424db74a59bd323e7e40182b8b9bec818868f
NinjaFlor <[200~Repository>
2024-11-02 17:05:25 -0300

    Mudança da ordem no código

--- src/Usuario.java
+++ src/Usuario.java
@@ -4,25 +4,31 @@ public class Usuario {
         SmartTv smartTv = new SmartTv();
 
         System.out.println("Canal Atual: " + smartTv.canal);
-
-        smartTv.mudarCanal(13);
-        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("---------------------------------------------");
 
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.aumentarVolume();
         System.out.println("Volume Atual: " + smartTv.volume);
+        System.out.println("---------------------------------------------");
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("---------------------------------------------");
 
         System.out.println("TV ligada? " + smartTv.ligada);
         System.out.println("Canal Atual: " + smartTv.canal);
         System.out.println("Volume Atual: " + smartTv.volume);
+        System.out.println("---------------------------------------------");
 
         smartTv.ligar();
         System.out.println("Novo Status -> TV ligada? " + smartTv.ligada);
+        System.out.println("---------------------------------------------");
 
         smartTv.desligar();
         System.out.println("Novo Status -> TV ligada? " + smartTv.ligada);
+        System.out.println("---------------------------------------------");
 
     }
 }
